95110e16a3fa519dce69307638cb7391c75a6310
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-17 07:39:31 +0000

    Automatically backed up by Learn

--- db/migrate/20201017073741_create_riders.rb
+++ db/migrate/20201017073741_create_riders.rb
@@ -1,6 +1,9 @@
-class CreateRiders < ActiveRecord::Migration[5.2]
+class CreateRides < ActiveRecord::Migration[5.2]
   def change
-    create_table :riders do |t|
+    create_table :rides do |t|
+      t.integer :user_id
+      t.integer :attraction_id
+      t.timestamps null: false
     end
   end
 end
